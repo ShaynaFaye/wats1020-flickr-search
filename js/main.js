ecc97880cf7a2aea6181c9061ef5b81d93f30951
Shayna Worthen <worthensf@gmail.com>
2016-03-19 01:19:32 -0700

    added comments for notation

--- js/main.js
+++ js/main.js
@@ -4,13 +4,7 @@
 // method. Use this to allow users to search for a tag or comma-separated list
 // of tags and view the images that are found.
 //
-// Allow users to click the images to see a larger version with more information.
-
-    
-
-    // Create a function called `searchImages()`. This function will handle the
-    // process of taking a user's search terms and sending them to Flickr for a
-    // response.
+   
 
     // Inside the `searchImages()` function, the following things should happen:
 
@@ -26,44 +20,54 @@
         //
         // 4.   Update the display to add the images to the list with the id
         //      `#images`.
+        
+        
 $(document).on('ready', function() {
-    
+    // Create a function called `searchImages()` that accepts a string value called `tags` as an argument:
     function searchImages(tags) {
+        // define location of the Flickr API:
         var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        // construct `$.getJSON()` call where you send a request object
+        // including the tags the user submitted
         $.getJSON( flickerAPI, {
             tags: tags,
             tagmode: "any",
             format: "json"
         })
+        
+        // construct a `done()` handler that displays images and accompanying info
         .done(function( data ) {
             $.each( data.items, function( i, item ) {
                 $( "#loading").hide();
-                var li = $("<li>").addClass("image-block");
+                var li = $("<li>").addClass("image-block"); //add new li for each new photo
                 
-                //$( "<img>" ).attr( "src", item.media.m ).appendTo( li );
-                var dl = $( "<dl>");
+                /* $( "<img>" ).attr( "src", item.media.m ).appendTo( li ); */
+                var dl = $( "<dl>"); // create definition list to display data to accompany each photo
+                // Add 'Title' and the photo's title
                 $("<dt>").html("Title").appendTo(dl);
                 $("<dd>").html(item.title).appendTo(dl);
+                // Add 'Date Taken' and the photo's date taken
                 $("<dt>").html("Date Taken").appendTo(dl);
                 $("<dd>").html(item.date_taken).appendTo(dl);
+                // Add 'Author' and the photo's author
                 $("<dt>").html("Author").appendTo(dl);
                 $("<dd>").html(item.author).appendTo(dl);
                 
-                /* I noticed when I added the description below, it adds the photo so I didn't need to add the image above myself */
+                // I noticed when I added the description below, it adds the photo (photo is part of the description information) 
+                // so I didn't need to add the image above myself 
                 $("<dt>").html("Description").appendTo(dl);
                 $("<dd>").html(item.description).appendTo(dl);
+                // Add 'Link' and a link to take user to the Flickr page of photo
                 $("<dt>").html("Link").appendTo(dl);
                 $("<dd>").html( "<a href='" + item.link + "' >View on Flickr</a>").appendTo(dl);
                 dl.appendTo(li);
                 li.appendTo("#images");
-            });
-        });
-    };
+            });  // end .each function
+        });  // end .done function
+    }; // end searchImages function
     
-    // Attach an event to the search button (`button.search`) to execute the
-    // search when clicked.
-
-        // When the Search button is clicked, the following should happen:
+    
+        // TODO: When the Search button is clicked, the following should happen:
         //
         // 1.   Prevent the default event execution so the browser doesn't
         //      Example: `event.preventDefault();`
@@ -74,15 +78,17 @@ $(document).on('ready', function() {
         // 3.   Execute the `searchImages()` function to fetch images for the
         //      user.
     
+     // Attach an event to the search button (`button.search`) to execute the
+    // search when clicked:
     $( "#search-form" ).on('submit', function( event ) {
         event.preventDefault();
-        /* Here's my thought process to guide me:
+        /* Here's my thought process to guide me in creating the steps below:
         *  1. grab text in search bar
         *  2. make sure it's not empty
         *  3. if emtpy, display error message to user
         *  4. clear out any previous content
         *  5. show loading icon
-        *  6. searchImages function
+        *  6. run searchImages function
         *  7. clear out search bar
         */
         var tags = $( "#search-input");
@@ -95,10 +101,10 @@ $(document).on('ready', function() {
         $( "#loading").show();
         searchImages(tags.val());
         tags.val("");
-    });
+    });  //end .on submit function
     
     
-});
+}); //end .on ready function
 
 
 
